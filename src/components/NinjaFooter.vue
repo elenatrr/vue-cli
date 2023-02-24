46b0a970c52167355234c14799eef4833758c555
Elena Tropanets <93614798+elenatrr@users.noreply.github.com>
2023-02-24 09:56:32 +0100

    lesson-22

---
+++ src/components/NinjaFooter.vue
@@ -0,0 +1,26 @@
+<template>
+  <footer>
+    <p>{{ copyright }}</p>
+  </footer>
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      copyright: 'Copyright 2023 Vue Ninjas'
+    }
+  }
+}
+</script>
+
+<style scoped>
+footer {
+  background-color: #222;
+  padding: 6px;
+}
+p {
+  color: aquamarine;
+  text-align: center;
+}
+</style>
