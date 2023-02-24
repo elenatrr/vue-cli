c5649a4c2ef4dc2cdb3b13db346bf03cf06bb293
Elena Tropanets <93614798+elenatrr@users.noreply.github.com>
2023-02-24 12:27:01 +0100

    lesson-23

---
+++ src/components/NinjaHeader.vue
@@ -0,0 +1,37 @@
+<template>
+  <header>
+    <h1 @click="changeTitle">{{ title }}</h1>
+  </header>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+    }
+  },
+  methods: {
+    changeTitle() {
+      this.title = "Vue Wizards"
+    }
+  }
+}
+</script>
+
+<style scoped>
+header {
+  background-color:aquamarine;
+  padding: 10px;
+}
+
+h1 {
+  color: #222;
+  text-align: center;
+}
+</style>
