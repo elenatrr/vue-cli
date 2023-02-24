41e901a983fa51ba3c09626a618872e3b44d7d7a
Elena Tropanets <93614798+elenatrr@users.noreply.github.com>
2023-02-24 09:29:09 +0100

    lesson-21

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ninja-header></ninja-header>
+    <ninja-main></ninja-main>
+    <ninja-footer></ninja-footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NinjaHeader from './components/NinjaHeader.vue'
+import NinjaMain from './components/NinjaMain.vue'
+import NinjaFooter from './components/NinjaFooter.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    'ninja-header': NinjaHeader,
+    'ninja-main': NinjaMain,
+    'ninja-footer': NinjaFooter
   }
 }
 </script>
 
 <style>
 #app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: -8px;
 }
 </style>
---
+++ src/components/NinjaHeader.vue
@@ -0,0 +1,27 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+  </header>
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      title: 'Vue Ninjas'
+    }
+  }
+}
+</script>
+
+<style scoped>
+header {
+  background-color:aquamarine;
+  padding: 10px;
+}
+
+h1 {
+  color: #222;
+  text-align: center;
+}
+</style>
