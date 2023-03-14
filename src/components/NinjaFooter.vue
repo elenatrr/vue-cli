07a8b5459773d797e13911f77ab44f183d7dba8d
Elena Tropanets <93614798+elenatrr@users.noreply.github.com>
2023-03-14 11:18:41 +0100

    lesson-25

---
+++ src/components/NinjaFooter.vue
@@ -0,0 +1,39 @@
+<template>
+  <footer>
+    <p>{{ copyright }} {{ title }}</p>
+  </footer>
+</template>
+
+<script lang="ts">
+import { bus } from '../main';
+
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+      copyright: 'Copyright 2023'
+    }
+  },
+  created() {
+    bus.$on('titleChanged', (data) => {
+      this.title = data;
+    })
+  }
+}
+</script>
+
+<style scoped>
+footer {
+  background-color: #222;
+  padding: 6px;
+}
+p {
+  color: aquamarine;
+  text-align: center;
+}
+</style>
